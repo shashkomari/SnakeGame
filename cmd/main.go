4e8bdcbddce6e315a91f0c0b3b4efa0fc152e3b3
Mariana Shashko <shashkomari@gmail.com>
2022-08-11 14:56:38 +0400

    Add config file functionality

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,45 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/shashkomari/SnakeGame/internal/snakegame"
 )
 
+type Config struct {
+	Board struct {
+		Hight int `json:"hight"`
+		Wight int `json:"width"`
+	} `json:"board"`
+}
+
+func LoadConfiguration(filename string) (Config, error) {
+	var config Config
+
+	// configFile, err := os.Create("config.json")
+	configFile, err := os.Open(filename)
+
+	if err != nil {
+		return config, err
+	}
+
+	defer configFile.Close()
+	// jsonFile := json.NewEncoder(configFile)
+
+	jsonParser := json.NewDecoder(configFile)
+	err = jsonParser.Decode(&config)
+	// config.Board.Hight = 10
+	// config.Board.Wight = 20
+	// err = jsonFile.Encode(&config)
+	return config, err
+}
+
 func main() {
-	var sg = snakegame.CreateSnakeGame(10, 20)
+	config, _ := LoadConfiguration("config.json")
+
+	var sg = snakegame.CreateSnakeGame(config.Board.Hight, config.Board.Wight)
 	exit := make(chan struct{}, 1)
 	turn := make(chan int, 1)
 
